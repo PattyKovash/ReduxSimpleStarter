602cee2c699f6f68de1f3ed2df081a91e03bdc0f
Patty Kovash <pattykovash@gmail.com>
2018-03-07 11:29:53 -0700

    Create and export SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const YOUTUBE_KEY = process.env.YOUTUBE_KEY;
 
 // Create new component. Componet should produce HTML
 
 const App = function () {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take component's generated HTML and add it to the DOM (add to page)
