6a3c9352e6967e13314209d90199c9ad0e4658b7
Patty Kovash <pattykovash@gmail.com>
2018-06-23 15:57:19 -0600

    Install ESLint

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={ this.state.term }
+          onChange={ this.onInputChange }/>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-
-const YOUTUBE_KEY = process.env.YOUTUBE_KEY;
-
-// Create new component. Componet should produce HTML
-
+// Create new component. This should produce some html
 const App = function () {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  return <div>Hi!</div>;
 }
+// Take this component's generated html and put in the DOM
 
-// Take component's generated HTML and add it to the DOM (add to page)
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import $ from 'jquery';
+// import SearchBar from './components/search_bar';
+// import YT from './config/config';
 
-ReactDOM.render(<App />, document.getElementById('container'));
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
 
+//     this.state = { videos: [] };
+//   }
+
+//   componentDidMount() {
+//     console.log(process.env);
+//     this.YTSearch('javascript');
+//   }
+
+//   YTSearch(term, callback) {
+//     $.ajax({
+//         url: 'https://www.googleapis.com/youtube/v3/search',
+//         method: 'GET',
+//         dataType: 'json',
+//         data: {
+//           key: window.YOUTUBE_API_KEY,
+//           part: 'snippet',
+//           q: term,
+//           type: 'video',
+//           maxResults: '25',
+//           videoEmbeddable: 'true'
+//         }
+//     })
+//       .done(({ items }) => {
+//         if (callback) {
+//           callback(items);
+//         }
+//         console.log('item: ', items);
+//       })
+//       .fail((err) => {
+//         console.log('error: ', err);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <SearchBar />
+//       </div>
+//     );
+//   }
+// };
+
+// ReactDOM.render(<App />, document.getElementById('container'));
